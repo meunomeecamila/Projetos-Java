a8de315b33fc9fd5e453e85353fb5459aaa727b1
Camila <contatocamilamenezes@gmail.com>
2025-10-15 16:43:31 -0300

    Inicio ordenacao

--- estudos/estudosp2/Estudocompleto.java
+++ estudos/estudosp2/Estudocompleto.java
@@ -26,3 +26,59 @@
 - Coleta de lixo 
 - Buscas parciais
 */
+
+//* Começo das matérias 
+// ! Selection sort 
+/* Complexidade no melhor caso: n²
+   Complexidade no pior caso: n²
+   Utilização: Bom para achar o menor item ou para ordenação parcial. 
+*/
+
+public void Selection(){
+    int[] vetor = {3, 5, 1, 0, 9, 8, 10};
+    int n = vetor.length;
+    for(int i=0; i<n; i++){
+        int menor = i;
+        for(int j=i+1; j<n;j++){
+            if(vetor[j] < vetor[menor]) menor = j;
+        }
+        swap(vetor,menor,i); //funcao que normalmente pode ter
+    }
+}
+
+// ! Insertion sort 
+/* Complexidade no melhor caso: n
+   Complexidade no pior caso: n²
+   Utilização: Bom para inserir elementos em um conjunto já ou parcialmente ordenado
+*/
+
+public void Insertion(){
+    int[] vetor = {3, 5, 1, 0, 9, 8, 10};
+    int n = vetor.length;
+    for(int i=1;i<n;i++){
+        int tmp = vetor[i];
+        int j = i-1;
+
+        while(j>=0 && vetor[j] > tmp){
+            vetor[j+1] = vetor[j]; //desloca
+            j--;
+        }
+
+        vetor[j+1] = tmp;
+    }
+}
+
+// ! Bubble sort 
+/* Complexidade no melhor caso: n
+   Complexidade no pior caso: n²
+   Utilização: Fácil de implementar e eficiente em casos pequenos
+*/
+public void Bubble() {
+    int[] vetor = {3, 5, 1, 0, 9, 8, 10};
+    int n = vetor.length;
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-1-i; j++){
+            if(vetor[j] > vetor[j+1]) swap(vetor, j, j+1);
+        }
+    }
+}
