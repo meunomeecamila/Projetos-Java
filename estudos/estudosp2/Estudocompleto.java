f9722485a065caf6f0a490e04fcb5a52acea5823
Camila <contatocamilamenezes@gmail.com>
2025-10-15 17:22:39 -0300

    Quick e shell

--- estudos/estudosp2/Estudocompleto.java
+++ estudos/estudosp2/Estudocompleto.java
@@ -2,13 +2,13 @@
 // ! Matérias:
 // ? (Códigos de ordenação) 
 /*
-- Selection
-- Bubble
-- Insertion
+- Selection - ok
+- Bubble - ok
+- Insertion - ok
 - Heap
-- Quick
+- Quick - ok
 - Merge 
-- Shell
+- Shell - ok
 - Counting
 */
 
@@ -81,4 +81,87 @@ public void Bubble() {
             if(vetor[j] > vetor[j+1]) swap(vetor, j, j+1);
         }
     }
+}
+
+// ! Quicksort
+/* Complexidade no melhor caso: n x lg n
+   Complexidade no pior caso: n²
+   Utilização: Muito rápido e usa pouca memória
+*/
+
+public void AuxiliarQuick(){
+    int[] vetor = {3, 5, 1, 0, 9, 8, 10};
+    int n = vetor.length;
+    Quicksort(0,n-1,vetor);
+}
+
+public void Quicksort(int esq, int dir, int[] vetor){
+    int i = esq, j = dir;
+    int pivo = vetor[(esq+dir)/2]; //meio
+    
+    while(i<=j){
+        while(vetor[i]<pivo)i++;
+        while(vetor[j]>pivo)j--;
+        if(i<=j){
+            swap(vetor,i,j);
+            i++; j--;
+        }
+    }
+
+    if (esq < j) Quicksort(esq, j, vetor);  // Recursão na parte esquerda
+    if (i < dir) Quicksort(i, dir, vetor);  // Recursão na parte direita
+}
+
+// ! Shellsort 
+/* Complexidade no melhor caso: n x log² n
+   Complexidade no pior caso: n²
+   Utilização: Melhoria do insertion, desloca menos quando está longe
+*/
+
+public void Shellsort(){
+    int[] vetor = {3, 5, 1, 0, 9, 8, 10};
+    int n = vetor.length;
+
+    //gap
+    for(int gap = n/2; gap > 0; gap/=2){
+        for(int i=gap; i<n; i++){
+            int tmp = vetor[i];
+            int j = i;
+
+            while(j>=gap && vetor[j-gap] > tmp){
+                vetor[j] = vetor[j-gap];
+                j-=gap; 
+            }
+
+            vetor[j] = tmp;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO - Funções auxiliares 
+//TODO - SWAP
+public static void swap(int i, int j, int[] array) {
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
