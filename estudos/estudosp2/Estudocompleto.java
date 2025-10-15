1f824836b3359d5207e016d53a16e3896c6f614c
Camila <contatocamilamenezes@gmail.com>
2025-10-15 16:05:29 -0300

    Lista das materias

--- estudos/estudosp2/Estudocompleto.java
+++ estudos/estudosp2/Estudocompleto.java
@@ -1,7 +1,28 @@
-//Esse documento conterá as matérias para a prova 2 de AED. 
-/*Matérias:
-(Códigos de ordenação)
+// * Esse documento conterá as matérias para a prova 2 de AED. 
+// ! Matérias:
+// ? (Códigos de ordenação) 
+/*
+- Selection
+- Bubble
+- Insertion
+- Heap
+- Quick
+- Merge 
+- Shell
+- Counting
+*/
 
-(Estruturas de dados flexíveis)
+// ? (Estruturas de dados flexíveis)
+/*
+- Pilha flex
+- Fila flex
+- lista simples flex
+- Lista dupla flex 
+- Matriz flex 
+*/
 
+// ? (Extras)
+/*
+- Coleta de lixo 
+- Buscas parciais
 */
