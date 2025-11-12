9348612aa673dd50767c92fbc2ba2627c0ad01b8
Camila <contatocamilamenezes@gmail.com>
2025-11-12 17:07:22 -0300

    estudos avl

--- estruturas/flexiveis/ArvoreAVL2.java
+++ estruturas/flexiveis/ArvoreAVL2.java
@@ -1,7 +1,7 @@
 class No {
     public int elemento;
     public No esq, dir;
-    private int nivel;
+    private int nivel; //será usado para calcular o fator
 
     public No(int elemento) {
         this(elemento, null, null);
@@ -11,19 +11,24 @@ class No {
         this.elemento = elemento;
         this.esq = esq;
         this.dir = dir;
-        this.nivel = 1;
+        this.nivel = 1; //começa com 1 pois é o próprio nivel
+        //obs: além disso, o pai precisaria de pelo menos 1 pra somar 1
     }
 
     public static int getNivel(No no) {
-        return (no == null) ? 0 : no.nivel;
+        if(no == null) return 0;
+        else return no.nivel;
     }
 
     public void setNivel() {
+        //o nivel será 1(do próprio nó) + o maior entre esq e dir
         this.nivel = 1 + Math.max(getNivel(esq), getNivel(dir));
     }
 }
 
 public class AVL {
+    /*A árvore AVL é uma árvore binária de pesquisa em que o fator de cada nó só
+    pode ser igual a 0, 1 ou -1 */
     private No raiz;
 
     public AVL() {
@@ -33,6 +38,8 @@ public class AVL {
     public boolean pesquisar(int x) {
         return pesquisar(x, raiz);
     }
+
+    // FUNÇÃO DE PESQUISAR TRADICIONAL ================
 
     private boolean pesquisar(int x, No i) {
         if (i == null) {
@@ -45,6 +52,8 @@ public class AVL {
             return pesquisar(x, i.dir);
         }
     }
+
+    //FUNÇÕES DE CAMINHAR TRADICIONAIS =============
 
     public void caminharCentral() {
         System.out.print("[ ");
@@ -88,6 +97,8 @@ public class AVL {
         }
     }
 
+    //FUNÇÃO DE INSERIR BALANCEADA ============
+
     public void inserir(int x) throws Exception {
         raiz = inserir(x, raiz);
     }
@@ -102,8 +113,10 @@ public class AVL {
         } else {
             throw new Exception("Erro ao inserir!");
         }
-        return balancear(i);
+        return balancear(i); //mudança
     }
+
+    //FUNÇÃO DE REMOVER BALANCEADA ============
 
     public void remover(int x) throws Exception {
         raiz = remover(x, raiz);
@@ -123,9 +136,10 @@ public class AVL {
         } else {
             i.esq = maiorEsq(i, i.esq);
         }
-        return balancear(i);
+        return balancear(i); //mudança
     }
 
+    //Função para achar o maior filho entre dois
     private No maiorEsq(No i, No j) {
         if (j.dir == null) {
             i.elemento = j.elemento;
@@ -136,31 +150,44 @@ public class AVL {
         return j;
     }
 
+    //FUNÇÕES PARA BALANCEAR ===========
     private No balancear(No no) throws Exception {
         if (no != null) {
+            //fator(i) = hdir(i) = hesq(i)
             int fator = No.getNivel(no.dir) - No.getNivel(no.esq);
 
-            if (Math.abs(fator) <= 1) {
+            if (Math.abs(fator) <= 1) { //pegar o módulo
                 no.setNivel();
-            } else if (fator == 2) {
+            } 
+            
+            else if (fator == 2) { //desbalanceado pra direita
                 int fatorFilho = No.getNivel(no.dir.dir) - No.getNivel(no.dir.esq);
-                if (fatorFilho == -1) {
+            
+                if (fatorFilho == -1) { //rotação dupla
                     no.dir = rotacionarDir(no.dir);
                 }
-                no = rotacionarEsq(no);
-            } else if (fator == -2) {
+                no = rotacionarEsq(no); //rotação simples
+            } 
+            
+            else if (fator == -2) { //desbalanceada para esquerda
                 int fatorFilho = No.getNivel(no.esq.dir) - No.getNivel(no.esq.esq);
-                if (fatorFilho == 1) {
+
+                if (fatorFilho == 1) { //rotação dupla
                     no.esq = rotacionarEsq(no.esq);
                 }
-                no = rotacionarDir(no);
-            } else {
+                no = rotacionarDir(no); //rotação simples
+            } 
+            
+            /*OBS: Se o código estiver funcionando corretamente, ele não
+            entrará nesse else NUNCA, pois o fator não passará de 2 ou de -2 */
+            else {
                 throw new Exception("Fator invalido!");
             }
         }
         return no;
     }
 
+    //Rotacionar a direita dupla -> significa que é um V desbalanceado p esquerda
     private No rotacionarDir(No no) {
         No noEsq = no.esq;
         No noEsqDir = noEsq.dir;
@@ -173,6 +200,7 @@ public class AVL {
         return noEsq;
     }
 
+    //Rotacionar a esquerda dupla -> significa que é um V desbalanceado p direita
     private No rotacionarEsq(No no) {
         No noDir = no.dir;
         No noDirEsq = noDir.esq;
