b70097085e6441bfe9c2ffdcee406676c32abd71
Camila <contatocamilamenezes@gmail.com>
2025-11-24 15:30:24 -0300

    inicio estudos p3

---
+++ estudos/estudosp3/EstudoCompleto.java
@@ -0,0 +1,70 @@
+//* ESTUDO COMPLETO PROVA TEÓRICA 3
+/*Esse arquivo abordará majoritariamente conteúdo de Árvores, tabelas hash
+e matérias adjacentes de finalização da disciplina de AED2 */
+
+//* Matérias p3
+/* 
+- Árvores binárias de busca (AVL)
+- Balanceamento de árvores binárias
+- Árvores 234
+- Árvores bicolor (alvinegras)
+- Tabela hash
+ */
+
+//! Árvores binárias de busca (AVL)
+/* São estruturas com uma raiz e nós, onde cada nó tem 0,1 ou 2 filhos (binárias). 
+Os filhos da sua esquerda são sempre menores e os da sua direita, maiores que o nó. 
+É considerada balanceada quando a diferença de altura entre a direita e a esquerda
+em cada nó é no máximo 1 ou -1. 
+Sua inserção, remoção e busca vai até no máximo lg n
+*/
+
+public class No {
+    public No esq; 
+    public No dir;
+    public int elemento;
+
+    //construtor
+    public No(){
+        this(0, null, null);
+    }
+
+    public No(int x){
+        this.elemento = x;
+        this.esq = null;
+        this.dir = null;
+    }
+}
+
+public class AVL {
+    public No raiz; 
+
+    //construtor
+    public AVL(){
+        this.raiz = null;
+    }
+
+    //caminhar central 
+    //lista todos os elementos em ordem crescente
+    public void caminharcentral(No i){
+        caminharcentral(i.esq);
+        System.out.println(i.elemento); //ou outra função
+        caminharcentral(i.dir);
+    }
+
+    //caminhar pré-ordem
+    //lista primeiro o nó e depois seus filhos
+    public void caminharpre(No i){
+        caminharpre(i.esq);
+        caminharpre(i.dir);
+        System.out.println(i.elemento); //ou outra função
+    }
+
+    //caminhar pós-ordem
+    //lista primeiro os filhos e por último o nó
+    public void caminharpos(No i){
+        System.out.println(i.elemento); //ou outra função
+        caminharpos(i.esq);
+        caminharpos(i.dir);
+    }
+}
